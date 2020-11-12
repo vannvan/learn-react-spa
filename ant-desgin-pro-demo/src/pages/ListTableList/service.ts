d10b9e6dd878125e02715f3a4146cccc69f7c5a9
vannvan <862670198@qq.com>
2020-11-12 21:19:39 +0800

    update

--- ant-desgin-pro-demo/src/pages/ListTableList/service.ts
+++ ant-desgin-pro-demo/src/pages/ListTableList/service.ts
@@ -2,37 +2,37 @@ import { request } from 'umi';
 import { TableListParams, TableListItem } from './data.d';
 
 export async function queryRule(params?: TableListParams) {
-  return request('/api/rule', {
-    params,
-  });
+    return request('/api/rules', {
+        params,
+    });
 }
 
 export async function removeRule(params: { key: number[] }) {
-  return request('/api/rule', {
-    method: 'POST',
-    data: {
-      ...params,
-      method: 'delete',
-    },
-  });
+    return request('/api/rules', {
+        method: 'POST',
+        data: {
+            ...params,
+            method: 'delete',
+        },
+    });
 }
 
 export async function addRule(params: TableListItem) {
-  return request('/api/rule', {
-    method: 'POST',
-    data: {
-      ...params,
-      method: 'post',
-    },
-  });
+    return request('/api/rules', {
+        method: 'POST',
+        data: {
+            ...params,
+            method: 'post',
+        },
+    });
 }
 
 export async function updateRule(params: TableListParams) {
-  return request('/api/rule', {
-    method: 'POST',
-    data: {
-      ...params,
-      method: 'update',
-    },
-  });
+    return request('/api/rule', {
+        method: 'POST',
+        data: {
+            ...params,
+            method: 'update',
+        },
+    });
 }
--- umi-app/src/pages/comp/hooks.tsx
+++ umi-app/src/pages/comp/hooks.tsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import moment from 'moment';
+import { Button, Input } from 'antd';
 export default function() {
   const [nowTime, setNowTime] = useState(
     moment().format('YYYY年MM月DD日 ddd HH:mm'),
   );
-
+  const inputEl = useRef(null);
+  // useEffect可以写多个
   useEffect(() => {
     const timer = setInterval(() => {
       setNowTime(moment().format('YYYY年MM月DD日 ddd HH:mm:ss'));
@@ -12,7 +14,20 @@ export default function() {
     return () => {
       clearInterval(timer);
     };
-  }, [nowTime]);
+  }, [nowTime]); //如果为空数，表示不监听任何状态，如果指定具体的状态，表示只监听该状态
 
-  return <>{nowTime}</>;
+  return (
+    <>
+      {nowTime}
+      <Input type="text" ref={inputEl} />
+      <Button
+        type={'primary'}
+        onClick={() => {
+          console.log(inputEl.current);
+        }}
+      >
+        获取Ref
+      </Button>
+    </>
+  );
 }
